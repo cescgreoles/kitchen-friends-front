ff50a47177b260da69892104932a9da35fb27bc4
Cesc Greoles <antoniogreoles@Mac-Cesc-Greoles.local>
2024-02-11 18:45:06 +0100

    creation of the home page, all the scss of the page and the code of the jsx file

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../styles/HomePage.scss";
+import Logo from "../assets/El Sofregit.png";
 
-const Home = () => {
-  return <div>sisi</div>;
+const HomePage = () => {
+  return (
+    <div className="container">
+      <div className="content">
+        <div className="logo-container">
+          <img className="logo" src={Logo} alt="Logo" />
+        </div>
+        <div className="buttons-container">
+          <Link to="/register" className="button">
+            Registrar-se
+          </Link>
+          <Link to="/login" className="button">
+            Inicia Sessió
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default Home;
+export default HomePage;
