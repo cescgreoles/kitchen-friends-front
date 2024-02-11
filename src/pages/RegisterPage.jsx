d21dc1908f76e1eaf86943b6251b96cc6519c492
Cesc Greoles <antoniogreoles@Mac-Cesc-Greoles.local>
2024-02-12 00:45:15 +0100

    front of RegisterPage page, both in jsx and scss code. missing the linking of the register with the back. update of the background photo without margins and in the whole web.

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,8 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "../styles/RegisterPage.scss";
+import { useNavigate } from "react-router-dom";
 
 const RegisterPage = () => {
-  return <div>RegisterPage</div>;
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    accessCode: "",
+  });
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://tu-backend.com/registro",
+        formData
+      );
+      console.log(response.data);
+      navigate("/login");
+    } catch (error) {
+      console.error("Error al registrar:", error);
+    }
+  };
+
+  return (
+    <div className="container">
+      <div className="registration-box">
+        <h2>Registra't </h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="name">Nom</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Contrasenya</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="access-code">Codi d'accès</label>
+            <input
+              type="text"
+              id="access-code"
+              name="accessCode"
+              value={formData.accessCode}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <button type="submit">Registrar-se</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default RegisterPage;
